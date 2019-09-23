87c0affa32547f7884b98985b17402b9c6bc8071
Tadej Baša <tadej.basa@gmail.com>
2019-09-23 11:48:33 +0200

    Test.

--- lib/paper_trail/repo_client.ex
+++ lib/paper_trail/repo_client.ex
@@ -2,7 +2,7 @@ defmodule PaperTrail.RepoClient do
   @doc """
   Gets the configured repo module or defaults to Repo if none configured
   """
-  def repo, do: Application.get_env(:paper_trail, :repo, Repo) |> IO.inspect()
+  def repo, do: Application.get_env(:paper_trail, :repo, IConstruct.Repo) |> IO.inspect()
   def originator, do: Application.get_env(:paper_trail, :originator, nil)
   def strict_mode, do: Application.get_env(:paper_trail, :strict_mode, false)
 end
